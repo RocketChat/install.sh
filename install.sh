33693076893081c0c2f20932b53a02f691682592
lucia.guevgeozian <lucia.guevgeozian@rocket.chat>
2019-06-22 00:35:37 -0500

    fixes to install.sh, added --version to rocketctl help message

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 readonly ROCKETCTL_DOWNLOAD_URL="https://raw.githubusercontent.com/RocketChat/install.sh/rocketctl/rocketctl"
 readonly ROCKETCTL_DIRECTOTRY="/usr/local/bin"
 
-[ ${EUID} -ne 0 ] && echo "This script must be run as root. Cancelling" >&2; exit 1;
-if [ -z $ROCKETCTL_DIRECTOTRY/rocketctl ]; then
+[ ${EUID} -ne 0 ] && (echo "This script must be run as root. Cancelling" >&2; exit 1;)
+if [ ! -f "$ROCKETCTL_DIRECTOTRY/rocketctl" ]; then
     curl -L $ROCKETCTL_DOWNLOAD_URL -o /tmp/rocketctl
     if  [ $? != 0 ]; then
         echo "Error downloading rocketctl."
@@ -13,7 +13,7 @@ if [ -z $ROCKETCTL_DIRECTOTRY/rocketctl ]; then
         mv /tmp/rocketctl $ROCKETCTL_DIRECTOTRY/
         chmod 755 $ROCKETCTL_DIRECTOTRY/rocketctl
     fi
-    $ROCKETCTL_DIRECTOTRY/rocketctl install
+    $ROCKETCTL_DIRECTOTRY/rocketctl install $@
 else
     echo "You already have rocketctl installed, use rocketctl to manage your RocketChat installation."
     echo "Run rocketctl help for more info."
