7b4cb1750a069717e196c5c5008500634fd0603f
lucia.guevgeozian <lucia.guevgeozian@rocket.chat>
2019-06-28 16:18:33 -0500

    changing typo in DIRECTOTRY for install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 readonly ROCKETCTL_DOWNLOAD_URL="https://raw.githubusercontent.com/RocketChat/install.sh/rocketctl/rocketctl"
-readonly ROCKETCTL_DIRECTOTRY="/usr/local/bin"
+readonly ROCKETCTL_DIRECTORY="/usr/local/bin"
 
 [ ${EUID} -ne 0 ] && (echo "This script must be run as root. Cancelling" >&2; exit 1;)
-if [ ! -f "$ROCKETCTL_DIRECTOTRY/rocketctl" ]; then
+if [ ! -f "$ROCKETCTL_DIRECTORY/rocketctl" ]; then
     curl -L $ROCKETCTL_DOWNLOAD_URL -o /tmp/rocketctl
     if  [ $? != 0 ]; then
         echo "Error downloading rocketctl."
         exit 1
     else
-        mv /tmp/rocketctl $ROCKETCTL_DIRECTOTRY/
-        chmod 755 $ROCKETCTL_DIRECTOTRY/rocketctl
+        mv /tmp/rocketctl $ROCKETCTL_DIRECTORY/
+        chmod 755 $ROCKETCTL_DIRECTORY/rocketctl
     fi
-    $ROCKETCTL_DIRECTOTRY/rocketctl install $@
+    $ROCKETCTL_DIRECTORY/rocketctl install $@
 else
     echo "You already have rocketctl installed, use rocketctl to manage your RocketChat installation."
     echo "Run rocketctl help for more info."
