2dae51a0a980ca9a308b0374f2cf86d7c4a695b7
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-07-05 13:19:53 +0530

    v2/helpers: host check helpers

---
+++ helpers/host.bash
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+source "../b-log/b-log.sh"
+source "../errors/errors.bash"
+
+# @private
+init_os_details() {
+  [[ -r /etc/os-release ]] || print_no_release_information_file_found_error
+
+  source /etc/os-release
+  declare -g DISTRO="$ID"
+  declare -g DISTRO_VERSION="$VERSION_ID"
+  declare -g DISTRO_CODENAME="${VERSION_CODENAME:-}"
+}
+
+# @private
+check_ubuntu() {
+  case "$DISTRO_VERSION" in
+    18.04 | 18.10 | 19.04 | 19.19 | 20.04 | 20.10 | 21.04 | 21.10 | 22.04) ;;
+    *) print_distro_not_supported_error_and_exit $DISTRO_VERSION ;;
+  esac
+}
+
+# @private
+check_debian() {
+  case "$DISTRO_VERSION" in
+    9 | 10 | 11) ;;
+    *) print_distro_not_supported_error_and_exit $DISTRO_VERSION ;;
+  esac
+}
+
+# @private
+check_centos() {
+  case "$DISTRO_VERSION" in
+    7 | 8) ;;
+    *) print_distro_not_supported_error_and_exit $DISTRO_VERSION ;;
+  esac
+}
+
+# @public
+verify_host() {
+  init_os_details
+  declare -A host_check=([ubuntu]=check_ubuntu [centos]=check_centos [debian]=check_debian)
+  local func=${host_check[$DISTRO]:-__no_func}
+  [[ $func == "__no_func" ]] && print_distro_not_supported_error_and_exit $DISTRO
+  eval $func
+}
