72371ab316731b65314d27129c7b4517530361fd
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-11-24 15:05:24 +0530

    update

--- main.bash
+++ main.bash
@@ -6,8 +6,8 @@ shopt -s extglob
 # TODO: better way of handling errors than ERROR::{exit 1;}
 
 _source() {
-  # shellcheck disable=1090
-  source "$(dirname "$(realpath "$0")")"/"${1?path is required}"
+	# shellcheck disable=1090
+	source "$(dirname "$(realpath "$0")")"/"${1?path is required}"
 }
 
 # TODO change this
@@ -19,50 +19,50 @@ _source "b-log/b-log.sh"
 _source "bash_concurrent/multiprocess.bash"
 
 handle_arguments() {
-  case "$1" in
-    "--help" | "-h")
-         show_long_help
-         exit 0
-                ;;
-    "install")
-      shift
-      is_host_supported
-      run_install "$@"
-                       ;;
-    "check-update" | "check-updates") ;;
-    "update") ;;
-    "upgrade-rocketchatctl") ;;
-    "configure") ;;
-    "backup") ;;
-    *) print_unknown_command_argument ;;
-  esac
+	case "$1" in
+		"--help" | "-h")
+			show_long_help
+			exit     0
+			;;
+		"install")
+			shift
+			is_host_supported
+			run_install  "$@"
+			;;
+		"check-update" | "check-updates") ;;
+		"update") ;;
+		"upgrade-rocketchatctl") ;;
+		"configure") ;;
+		"backup") ;;
+		*) print_unknown_command_argument ;;
+	esac
 }
 
 # TODO add --dry-run option
 
 entrypoint() {
-  # TODO change this
-  if ! am_i_root; then
-    FATAL "you must use a non-root account for this script to work"
-    exit 1
-  fi
+	# TODO change this
+	if ! am_i_root; then
+		FATAL "you must use a non-root account for this script to work"
+		exit 1
+	fi
 
-  SUCCESS "using non-root user"
+	SUCCESS "using non-root user"
 
-  handle_arguments "$@"
+	handle_arguments "$@"
 }
 
 # shellcheck disable=2015
 [[ -n "$DEBUG" ]] &&
-  LOG_LEVEL_DEBUG ||
-  LOG_LEVEL_INFO
+	LOG_LEVEL_DEBUG ||
+	LOG_LEVEL_INFO
 
 LOG_LEVELS+=("250" "SUCCESS" "$B_LOG_DEFAULT_TEMPLATE" "\e[1;32m" "\e[0m")
 
 SUCCESS() {
-  # don't look at the log
-  # i want FUNCNAME to be just right
-  B_LOG_print_message 250 "${*?message required}"
+	# don't look at the log
+	# i want FUNCNAME to be just right
+	B_LOG_print_message 250 "${*?message required}"
 }
 
 concurrency_init -p "$(basename "$0")"
