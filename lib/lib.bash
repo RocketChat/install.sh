e2ca1e0f906f7961f67e50eba5f0eb6915810fa9
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-11-27 01:54:01 +0530

    wip(v2): more nodejs functions

--- lib/lib.bash
+++ lib/lib.bash
@@ -1,37 +1,91 @@
 #!/bin/bash
 
-[ -n "${__SOURCE_GUARD_LIB+@@@}" ] && return
+[[ -n "${__SOURCE_GUARD_LIB+@@@}" ]] && return
 export __SOURCE_GUARD_LIB=
 
-shopt -s expand_aliases
+# if testing most debug functions and func* functions are
+# going to be overwritten.
+# these redirects would conflict with bats.
+# shellcheck disable=2155
+[[ -z "${TEST_MODE+@@@}" ]] &&
+	exec 4>&1 &&
+	declare -g __func_returns="$(mktemp -p "${TMPDIR:-/tmp}" "__rocketchatctl__func_returnsXXXXX")" &&
+	exec 3>"$__func_returns" &&
+	trap 'rm -f '"'$__func_returns'"'' EXIT
 
-source "bash-argsparse/argsparse.sh"
+# shellcheck disable=1094
+source "$(dirname "$(realpath "$0")")"/bash-argsparse/argsparse.sh
 
 command_exists() {
-	local command="${1?command must be passed}"
-	command -v "$command" &>/dev/null
+	command -v "$1" &>/dev/null
 }
 
-# shellcheck disable=2154,2142
-#	(parse_arguments is referenced but not assigned)
-#	(aliases can't use positional arguments, use a function)
-alias parse_arguments='
-	argsparse_parse_options "$@";
-	local __argument_identifier __argument_identifier_stripped __argument_identifier_value;
-	local __eval_option_string
-	for __argument_identifier in "${!program_options[@]}"; do
-		__argument_identifier_stripped="${__argument_identifier//-/_}"
-		__argument_identifier_value="${program_options[${__argument_identifier}]}"
-		if argsparse_has_option_property "$__argument_identifier" value; then
-			__eval_option_string+="local ${__argument_identifier_stripped}=${__argument_identifier_value};"
-			continue
-		fi
-		__eval_option_string+="function $__argument_identifier_stripped { (($__argument_identifier_value)); };"
-	done
-	[[ -n "$__eval_option_string" ]] && eval "$__eval_option_string"
-'
-
 check_option_type_semver() {
-	local value="${1?version string required}"
-	[[ "$value" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+	[[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+}
+
+is_dir_accessible() {
+	local dir="${1?must pass a directory path}"
+	if ! [[ -d "$dir" ]]; then
+		ERROR "$dir is not a directory; failing"
+		return 1
+	fi
+	local \
+		uid \
+		gid \
+		dir_owner_uid \
+		dir_owner_gid \
+		dir_perm_oct
+	uid="$(id -u)"
+	DEBUG "uid: $uid"
+	gid="$(id -g)"
+	DEBUG "gid: $gid"
+	dir_owner_uid="$(stat -c "%u" "$dir")"
+	dir_owner_gid="$(stat -c "%g" "$dir")"
+	dir_perm_oct="$(stat -c "%a" "$dir")"
+	DEBUG "dir_owner_uid: $dir_owner_uid"
+	DEBUG "dir_owner_gid: $dir_owner_gid"
+	DEBUG "dir_perm_oct: $dir_perm_oct"
+	if {
+		((uid == dir_owner_uid)) && (((dir_perm_oct / 100) == 7))
+	} ||
+		{
+			((gid == dir_owner_gid)) && ((((dir_perm_oct % 100) / 10) == 7))
+		}; then
+		return 0
+	fi
+	# thanks bash; what I actually mean is ==7
+	return $((((dir_perm_oct % 100) % 10) != 7))
+}
+
+path_join() {
+	printf "%s/%s" "${1%/}" "$2"
+}
+
+funcrun() {
+	eval "$*" >&4
+	# wish I knew of a better way :/
+	tail -1 "$__func_returns"
+}
+
+funcreturn() {
+	echo "$@" >&3
+}
+
+path_append() {
+	local ifs="$IFS" path
+	IFS=:
+	for path in $PATH; do
+		[[ "$path" == "$1" ]] && return
+	done
+	IFS="$ifs"
+	export PATH="${1}:$PATH"
+}
+
+sudo() {
+	command sudo -E env PATH="$PATH" "$@"
+}
+
+node_confirm_version() {
+	node -e "process.exit(/${1:-.+}/.test(process.version) ? 0 : 1)"
 }
--- lib/nodejs.bash
+++ lib/nodejs.bash
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-[ -n "${__SOURCE_GUARD_NODEJS+@@@}" ] && return
+[[ -n "${__SOURCE_GUARD_NODEJS+@@@}" ]] && return
 export __SOURCE_GUARD_NODEJS=
 
-source "bash-argsparse/argsparse.sh"
+# HOW THE HECK DO I TEST THESE FUNCTIONS?:quiet
+source "$(dirname "$(realpath "$0")")"/bash-argsparse/argsparse.sh
+source "$(dirname "$(realpath "$0")")"/b-log/b-log.sh
 
-source "lib/lib.bash"
-source "b-log/b-log.sh"
+source "$(dirname "$(realpath "$0")")"/lib/lib.bash
 
+# @brief run nvm through a new bash shell
+# @details
+# `source $NVM_DIR/nvm.sh has proven to be problematic
+# @return 1 if BASH_ENV is empty
+nvm() {
+	[[ -z "$_BASH_ENV" ]] && return 1
+	BASH_ENV="$_BASH_ENV" bash -c "nvm $*"
+}
+
+# @brief install n
+# @details
+# Installs n; if node or npm is not found in PATH
+# this function fails verbosely.
+# @return 0 if successful 1 if not
 _install_n() {
-	# responsibility of checking for node and npm is of the caller's
+	if ! command_exists node && ! command_exists npm; then
+		FATAL "installing n requires some version of npm and nodejs already installed" \
+			", can't move forward"
+		return 1
+	fi
 	INFO "installing n"
+	if command_exists n; then
+		SUCCESS "n is already installed"
+		return 0
+	fi
 	if ! sudo npm i n -g; then
 		FATAL "failed to install n"
 		return 1
@@ -18,37 +41,122 @@ _install_n() {
 	SUCCESS "successfully installed n"
 }
 
+# @brief install nvm
+# @details
+# Install the nvm script. But does not source it.
+# Instead it adds a "wrapper" of sorts so that the following steps can call the
+# command directly. This way each call will have a fresh environment.
+# @param where to install the script
+# @return 1 if unsuccessful
 _install_nvm() {
-	:
+	local nvm_dir="$1"
+	DEBUG "nvm_dir: $nvm_dir"
+	INFO "installing nvm"
+	# my nvm function is not as long as the actual one :p
+	if (($(type nvm | wc -l) == 4)); then
+		nvm() { command nvm "@"; } # overwrite the BASH_ENV function as that's not needed anymore
+		SUCCESS "nvm is already installed"
+		return 0
+	elif [[ -d "$nvm_dir" ]]; then
+		export _BASH_ENV="$(path_join "$nvm_dir" "nvm.sh")"
+		SUCCESS "nvm is already installed"
+		return 0
+	elif [[ -d "$HOME/.nvm" ]]; then
+		export _BASH_ENV="$HOME/.nvm/nvm.sh"
+		SUCCESS "nvm is already installed"
+		return 0
+	fi
+	# TODO change version
+	local cmd=(git clone --quiet https://github.com/nvm-sh/nvm --depth 1 -b "v0.39.1" "$nvm_dir")
+	is_dir_accessible "$(dirname "$nvm_dir")" || cmd=(sudo "${cmd[@]}")
+	if ! ("${cmd[@]}" && sudo chown -R "$(id -u)":"$(id -g)" "$nvm_dir"); then
+		FATAL "failed to install nvm" \
+			"can not move forward with the installation anymore"
+		return 1
+	fi
+	DEBUG "setting temporary BASH_ENV for nvm commands"
+	export _BASH_ENV="$(path_join "$nvm_dir" "nvm.sh")"
+	SUCCESS "successfully installed nvm"
 }
 
+# @return path to installed nodejs binary
 _nvm_install_nodejs() {
-	:
+	local node_version="${1?node version is required}"
+	if ! nvm install "$node_version"; then
+		FATAL "failed to install $node_version using nvm"
+		return 1
+	fi
+	funcreturn "$(dirname "$(nvm which "$node_version")")" && return
+	ERROR "failed to capture installed node binary path"
+	WARN "falling back on /usr/local/bin/node"
+	funcreturn "/usr/local/bin"
 }
 
+# @return path to installed nodejs binary
 _n_install_nodejs() {
-	:
+	local node_version="${1?node version is required}"
+	if ! n install "$node_version"; then
+		FATAL "failed to install $node_version using n"
+		return 1
+	fi
+	funcreturn "$(dirname "$(n which "$node_version")")" && return
+	ERROR "failed to capture installed node binary path"
+	WARN "falling back on /usr/local/bin/node"
+	funcreturn "/usr/local/bin"
 }
 
+# @param node version, install path
 _manual_install_nodejs() {
-	:
+	local node_version="${1?node version is required}"
+	local install_path="${2?install path required}"
+	if command_exists node && node_confirm_version "$node_version"; then
+		SUCCESS "existing node installation version satisfied"
+		return 0
+	fi
+	local archive_file_name="node-v$node_version-linux-x64"
+	DEBUG "archive_file_name: $archive_file_name"
+	local url="https://nodejs.org/dist/v$node_version/$archive_file_name.tar.xz"
+	DEBUG "url: $url"
+	INFO "downloading node $node_version installation archive"
+	if ! (
+		cd /tmp
+		curl -sLO --fail "$url"
+	); then
+		FATAL "failed to download nodejs archive"
+		FATAL "cannot move on with installation without a valid node binary; exiting..."
+		return 1
+	fi
+	INFO "installing nodejs in $install_path"
+	# TODO handle directory creation better
+	# FIXME you know what
+	[[ -d "$install_path" ]] || sudo mkdir "$install_path"
+	if ! tar -xJf "/tmp/$archive_file_name.tar.xz" -C "$install_path"; then
+		FATAL "failed to extract archive; nodejs install failed"
+		FATAL "cannot move on with installation without a valid node binary; exiting..."
+		return 1
+	fi
+	local new_path="$(path_join "$install_path" "$(path_join "$archive_file_name" "bin")")"
+	DEBUG "new_path: $new_path"
+	funcreturn "$new_path"
 }
 
 install_nodejs() {
-	local node_version=
-	argsparse_use_option use-nvm "use nvm for installing nodejs"
-	argsparse_use_option use-n "use n for installing nodejs"
-	argsparse_use_option node-version: "required nodejs version" mandatory type:semver
-	parse_arguments
+	local node_version="${1?node version is required}"
+	# use_n and use_nvm should be available after top level parse_arguments
 	if use_n; then
-		_install_n
-		_n_install_nodejs "$node_version"
-		return
+		if ! { _install_n && _n_install_nodejs "$node_version"; }; then
+			FATAL "failed to install nodejs"
+			return 1
+		fi
+		return 0
 	fi
 	if use_nvm; then
-		_install_nvm
-		_nvm_install_nodejs "$node_version"
-		return
+		if ! { _install_nvm "/opt/nvm" && _nvm_install_nodejs "$node_version"; }; then
+			FATAL "failed to install nodejs"
+			return 1
+		fi
+		return 0
 	fi
-	_manual_install_nodejs "$node_version"
+	DEBUG "attempting manually installing nodejs v$node_version"
+	_manual_install_nodejs "$node_version" "/opt/nodejs"
 }
